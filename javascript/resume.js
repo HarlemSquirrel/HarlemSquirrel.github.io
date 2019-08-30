4259b2a72c1ea7c8b60c95b926cdf2c01bf89dd5
Kevin McCormack <harlemsquirrel@gmail.com>
2019-08-30 15:51:40 -0400

    Update resume style
    - Add PDF export
    - Use Material icons

---
+++ javascript/resume.js
@@ -0,0 +1,27 @@
+function formattedDate(date) {
+  // YYYYMMDD
+  return date.toISOString().split('T')[0].replace(/-/gi, '')
+}
+
+function print_resume() {
+  var date = new Date()
+  var element = document.querySelector('body')
+  // var element = document.querySelector('body').cloneNode(true)
+  var opt = {
+    margin:       0.6,
+    enableLinks:  true,
+    filename:     'kevin_mccormack_resume_' + formattedDate(date) + '.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 2 },
+    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+  };
+
+  var currentClass = element.className
+  element.className = "pdf"
+
+  html2pdf().set(opt).from(element).save().then(function () {
+    element.className = currentClass
+  })
+}
+
+document.getElementById('downloadPDF').onclick = print_resume
